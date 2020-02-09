670c3f2c03928867aedbdb2c9bd1edebc11f69da
izumin5210 <m@izum.in>
2020-02-09 22:26:05 +0900

    :memo:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   acts_as_paranoid column: :deleted, sentinel_value: false
+
+  # @!group Scopes
+
+  # @!method registered
+  #   Lists registered users.
+  #   @!scope class
+  #   @return [ActiveRecord::Relation]
   scope :registered, -> { where(registered: true) }
+
+  # @!endgroup
 end
